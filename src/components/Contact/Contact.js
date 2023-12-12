7bb8c16068e955d096036af21aa5bc30401a4f61
konrad-harezlak <konrad.harezlakk@gmail.com>
2023-12-12 17:41:10 +0100

    Fix contact css file

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -15,8 +15,8 @@ export default class Contact extends React.Component {
                 
                 <div className='bussines-card'>
                     <h1>Konrad Harężlak</h1>
-                    <div className='card-number-left-top'><p><FontAwesomeIcon icon={faPhone} /> <a href="tel:+48791019796">+48 791 019 796</a></p></div>
-                    <div className='card-adres-right-top'><p><FontAwesomeIcon icon={faHouse} /> <a href="https://www.google.com/maps/search/?q=Katowice+Kalinowa+39" target="_blank" rel="noopener noreferrer">Katowice Kalinowa 39</a></p></div>
+                    <div className='card-number-left-top'><p><FontAwesomeIcon icon={faPhone} /> <a href="tel:">000 000 000</a></p></div>
+                    <div className='card-adres-right-top'><p><FontAwesomeIcon icon={faHouse} /> <a href="https://www.google.com/maps/" target="_blank" rel="noopener noreferrer">Katowice</a></p></div>
                     <div className='card-email-left-bottom'><p><FontAwesomeIcon icon={faEnvelope} />  <a href="mailto:konrad.harezlakk@gmail.com" target="_blank">konrad.harezlakk@gmail.com</a></p></div>
                     <div className='card-page-right-bottomn'><p><FontAwesomeIcon icon={faGlobe} />  <a href="https://github.com/konrad-harezlak/" target="_blank" rel="noopener noreferrer">github.pl/konrad-harezlak</a></p></div>
                 </div>
